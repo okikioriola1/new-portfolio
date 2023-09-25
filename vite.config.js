4676da22e24d8c72ba34471c6a17f9aa23f5004b
Okiki Oriola <okikioriola1@gmail.com>
2023-09-25 14:09:31 +0100

    redirect fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import { ViteHtmlPluginOptions } from 'vite-plugin-html';
-const { ViteHtmlPluginOptions } = require('vite-plugin-html');
-const htmlPluginOptions = {
-  entries: {
-    index: './index.html',       // Entry point for index.html
-    about: './about.html',       // Entry point for about.html
-    projects: './projects.html'  // Entry point for projects.html
-  },
-};
-
-export default defineConfig({
-  plugins: [htmlPluginOptions],
-});
+export default {
+    build: {
+      rollupOptions: {
+        input: {
+          index: './index.html',
+          about: './about.html',
+          projects: './projects.html',
+          home:'./home.html'
+          // Add other HTML entry points as needed
+        },
+      },
+    },
+  };
